cdd9d856c70b35a8649bf03bbaf87cdd9fe1bfe6
irfanshafiq2002 <irfanshafiq.khairulhisham@gmail.com>
2022-10-25 22:03:19 +0800

    Update scriptlogin.js

--- scriptlogin.js
+++ scriptlogin.js
@@ -4,7 +4,7 @@ function validate()
     var filterA = /(?=.*[A-Z])/;
     var filtera = /(?=.*[a-z])/;
     var filterNum = /(?=.*[0-9])/;
-    var filterSym = /(?=.*\d)/;
+    var filterSym = /(?=.*[#,_^!@*&])/;
     if(password == "")
     {
         alert('Please Enter a Password');
@@ -17,19 +17,18 @@ function validate()
     {
         alert("The password need to include atleast ONE lowercase")
     }
+    else if(!filterNum.test(password))
+    {
+        alert("The password need to include atleast ONE number")
+    }
+    else if(!filterSym.test(password))
+    {
+        alert("The password need to include atleast ONE symbol")
+    }
     else if(password.length < 12)
     {
         alert("Sorry, your password is less than 12 characters")
     }
-    else if(!filterNum.text(password))
-    {
-        alert("The password need to include atleast ONE number")
-    }
-    else if(!filterSym.text(password))
-    {
-        alert("The password need to include atleast ONE symbol")
-    }
-   
     else
     {
         alert('Successful Login');
